96e22e376da3004a1ea43a784cfb9e84926040d8
Karina-Borodina <152685804+Karina-Borodina@users.noreply.github.com>
2024-06-11 09:43:37 +0500

    Add files via upload

---
+++ src/component/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi, my dear!</h1>;<h3>How are you?</h3>;
+      </div>
+    );
+  }
+}
+
+export default Welcome;
